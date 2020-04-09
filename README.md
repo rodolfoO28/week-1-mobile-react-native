24398c25ac7a3aa4f90ce7dff997ff807e9219cf
Rodolfo Oliveira <rdf2806@gmail.com>
2020-04-08 23:27:48 -0300

    Lesson of introduction of mobile using React Native

---
+++ README.md
@@ -0,0 +1,12 @@
+<img alt="GoStack" src="https://storage.googleapis.com/golden-wind/bootcamp-gostack/header-desafios.png" />
+
+# 🚀️ Week 1 - Mobile React Native
+
+Project of week 1, mobile introduction using React Native. Follow the topics covered below:
+
+- React Native Architecture
+- Configuration SDK
+- Create new project with React Native
+- Diferences between ReactJS
+- List projects of API
+- Add project using API
---
+++ src/index.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, StyleSheet, StatusBar, FlatList, TouchableOpacity } from 'react-native';
+
+import api from './services/api'
+
+export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      const data = response.data;
+      console.log(data);
+      
+      setProjects(data);
+    })
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Rodolfo Oliveira'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item }) => (
+            <Text style={styles.project}>{item.title}</Text>
+          )}
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>
+            Adicionar projeto
+          </Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+
+  project: {
+    fontSize: 32,
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+
+  button: {
+    backgroundColor: '#FFF',
+    borderRadius: 4,
+    margin: 20,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
